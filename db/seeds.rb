1b6c772831776cbd784af749defac2996880ff30
Weijun Du <weijundu883@163.com>
2018-05-13 00:45:57 +0100

    added table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: "title 1", details: "details 1")
+Task.create(title: "title 2", details: "details 2")
+Task.create(title: "title 3", details: "details 3")
